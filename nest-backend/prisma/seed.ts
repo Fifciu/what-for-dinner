91cfa1521d99415b03ffaaa905fdf251e7d52af2
Fifciuu <filip.jdrasik@gmail.com>
2024-09-03 17:53:42 +0200

    refactor: seeder

--- nest-backend/prisma/seed.ts
+++ nest-backend/prisma/seed.ts
@@ -1,71 +1,104 @@
-import { PrismaClient, UserInGroupRole } from '@prisma/client';
+import { Dish, PrismaClient, UserInGroupRole } from '@prisma/client';
 import { compare, hash } from 'bcrypt';
+import { group } from 'console';
 
 const prisma = new PrismaClient();
 const SALT_OR_ROUNDS = 10;
 
-async function main() {
+async function createUserAndAssignToGroup({ email, name }, groupId: number) {
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      password: await hash('zaq1@WSX', SALT_OR_ROUNDS),
+    },
+  });
+  await prisma.userInGroupRoles.create({
+    data: {
+      userId: user.id,
+      groupId,
+      role: UserInGroupRole.CHEF,
+    },
+  });
+  return user;
+}
+
+async function createGroupAndUsers() {
   const group1 = await prisma.group.create({
     data: {
       name: 'Wolanowska',
     },
   });
-  const user1 = await prisma.user.create({
-    data: {
-      email: 'yessica@fifciuu.com',
-      name: 'Sylwia',
-      password: await hash('zaq1@WSX', SALT_OR_ROUNDS),
-    },
+  const user1 = createUserAndAssignToGroup(
+    { email: 'yessica@fifciuu.com', name: 'Sylwia' },
+    group1.id,
+  );
+  const user2 = createUserAndAssignToGroup(
+    { email: 'me@fifciuu.com', name: 'Filip' },
+    group1.id,
+  );
+  const user3 = createUserAndAssignToGroup(
+    { email: 'iga@fifciuu.com', name: 'Iga' },
+    group1.id,
+  );
+  const user4 = createUserAndAssignToGroup(
+    { email: 'tomasz@fifciuu.com', name: 'Tomasz' },
+    group1.id,
+  );
+
+  return {
+    group: group1,
+    users: [user1, user2, user3, user4],
+  };
+}
+
+async function createDish(data: Omit<Dish, 'id'>) {
+  return await prisma.dish.create({
+    data,
   });
-  const ur1 = await prisma.userInGroupRoles.create({
-    data: {
-      userId: user1.id,
-      groupId: group1.id,
-      role: UserInGroupRole.CHEF,
+}
+
+function dishesFactory(groupId: number): Array<Omit<Dish, 'id'>> {
+  return [
+    {
+      groupId,
+      name: 'Makaron ze szpinakiem',
+      photoSrc: '',
     },
-  });
-  const user2 = await prisma.user.create({
-    data: {
-      email: 'me@fifciuu.com',
-      name: 'Filip',
-      password: await hash('zaq1@WSX', SALT_OR_ROUNDS),
+    {
+      groupId,
+      name: 'Pomidorowa',
+      photoSrc: '',
     },
-  });
-  const ur2 = await prisma.userInGroupRoles.create({
-    data: {
-      userId: user2.id,
-      groupId: group1.id,
-      role: UserInGroupRole.USER,
+    {
+      groupId,
+      name: 'Rosół',
+      photoSrc: '',
     },
-  });
-  const user3 = await prisma.user.create({
-    data: {
-      email: 'iga@fifciuu.com',
-      name: 'Iga',
-      password: await hash('zaq1@WSX', SALT_OR_ROUNDS),
+    {
+      groupId,
+      name: 'Barszcz Biały',
+      photoSrc: '',
     },
-  });
-  const ur3 = await prisma.userInGroupRoles.create({
-    data: {
-      userId: user3.id,
-      groupId: group1.id,
-      role: UserInGroupRole.USER,
+    {
+      groupId,
+      name: 'Gołąbki',
+      photoSrc: '',
     },
-  });
-  const user4 = await prisma.user.create({
-    data: {
-      email: 'tomasz@fifciuu.com',
-      name: 'Tomasz',
-      password: await hash('zaq1@WSX', SALT_OR_ROUNDS),
+    {
+      groupId,
+      name: 'Ryba, Frytki, Surówki',
+      photoSrc: '',
     },
-  });
-  const ur4 = await prisma.userInGroupRoles.create({
-    data: {
-      userId: user4.id,
-      groupId: group1.id,
-      role: UserInGroupRole.USER,
-    },
-  });
+  ];
+}
+
+async function main() {
+  const { group, users } = await createGroupAndUsers();
+  const dishes = dishesFactory(group.id);
+  for (const dish of dishes) {
+    await createDish(dish);
+  }
 }
 
 main()
